3ce78b7f7ecfce7cedf6b3f4ae111effa54f4739
Wrezlee <risky50putra@gmail.com>
2025-12-17 22:07:49 +0700

    emboh

--- User.py
+++ User.py
@@ -6,44 +6,89 @@ mydb = mysql.connector.connect(
     password="",
     database="pbo_uas"
 )
-mycursor=mydb.cursor()
+
+mycursor = mydb.cursor()
+
 
 class User:
 
     def __init__(self):
-        self
-  
-    def login(username,password):
-        sql = "SELECT level FROM user WHERE username=%s AND password=%s"
-        val = (username, password)
-        mycursor.execute(sql, val)
-        result = mycursor.fetchone()
-        return str(result[0])
-        
-    def insert_data(val1,val2,val3,val4):
-        sql="INSERT INTO user (nama,username,password,level) VALUES (%s,%s,%s,%s)"
-        val=(val1,val2,val3,val4)
-        mycursor.execute(sql,val)
-        mydb.commit()
-        print(mycursor.rowcount,"Data berhasil ditambahkan...")
+        pass
 
-    def select_data_by_id(val1):
-        sql = "SELECT nama,password,level FROM user WHERE username = %s"
-        val = (val1)
-        mycursor.execute(sql,val)
-        myresult = mycursor.fetchone()
-        return myresult
-    
-    def update_data(nama,username,password,level):
-        sql = "UPDATE user SET nama = %s, password = %s, level = %s WHERE username = %s"
-        val = (nama,username,password,level)
-        mycursor.execute(sql,val)
+    def get_all():
+        sql = """
+            SELECT id, username, nama_lengkap, level, password
+            FROM users
+        """
+        mycursor.execute(sql)
+        return mycursor.fetchall()
+
+    def search(keyword, level):
+        if level in ("", "Semua Level"):
+            sql = """
+                SELECT id, username, nama_lengkap, level, password
+                FROM users
+                WHERE nama_lengkap LIKE %s OR username LIKE %s
+            """
+            val = (f"%{keyword}%", f"%{keyword}%")
+        else:
+            sql = """
+                SELECT id, username, nama_lengkap, level, password
+                FROM users
+                WHERE (nama_lengkap LIKE %s OR username LIKE %s)
+                AND level = %s
+            """
+            val = (f"%{keyword}%", f"%{keyword}%", level.lower())
+
+        mycursor.execute(sql, val)
+        return mycursor.fetchall()
+
+    def insert_data(id_user, username, password, nama_lengkap, level):
+        sql = """
+            INSERT INTO users (id, username, password, nama_lengkap, level)
+            VALUES (%s, %s, %s, %s, %s)
+        """
+        val = (id_user, username, password, nama_lengkap, level.lower())
+        mycursor.execute(sql, val)
         mydb.commit()
-        print(mycursor.rowcount, "Data berhasil diupdate")
-        
-    def delete_data(val1):
-        sql = "DELETE FROM user WHERE username = %s"
-        value=(val1,)
-        mycursor.execute(sql, value)
+        print(mycursor.rowcount, "User berhasil ditambahkan")
+
+    def select_data_by_id(user_id):
+        sql = """
+            SELECT id, username, nama_lengkap, level
+            FROM users
+            WHERE id = %s
+        """
+        mycursor.execute(sql, (user_id,))
+        return mycursor.fetchone()
+
+    def update_data(user_id, username, nama_lengkap, password, level):
+        if password:
+            sql = """
+                UPDATE users
+                SET username=%s,
+                    nama_lengkap=%s,
+                    password=%s,
+                    level=%s
+                WHERE id=%s
+            """
+            val = (username, nama_lengkap, password, level.lower(), user_id)
+        else:
+            sql = """
+                UPDATE users
+                SET username=%s,
+                    nama_lengkap=%s,
+                    level=%s
+                WHERE id=%s
+            """
+            val = (username, nama_lengkap, level.lower(), user_id)
+
+        mycursor.execute(sql, val)
         mydb.commit()
-        print(mycursor.rowcount, "Data Berhasil dihapus...")
+        print(mycursor.rowcount, "User berhasil diupdate")
+
+    def delete_data(user_id):
+        sql = "DELETE FROM users WHERE id=%s"
+        mycursor.execute(sql, (user_id,))
+        mydb.commit()
+        print(mycursor.rowcount, "User berhasil dihapus")
